fdef8003ac33a9a3a5e030a3715f99ffcc363dae
Eddylucero <luceroeddy8@gmail.com>
2025-05-22 13:23:59 -0500

    Migracion y creacion de los models de Tienda

--- Aplicaciones/Tienda/models.py
+++ Aplicaciones/Tienda/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Tienda(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    fecha = models.DateField()
